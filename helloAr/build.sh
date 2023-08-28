cecafb3735c9220709eba68e841765364e45de95
jadon6uwizeye <jeandedieuuwizeye6@gmail.com>
2023-08-28 13:17:08 +0200

    update build.sh

--- helloAr/build.sh
+++ helloAr/build.sh
@@ -8,5 +8,5 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 # create superuser
-DJANGO_SUPERUSER_PASSWORD=1234567890
-python manage.py createsuperuser --no-input --username admin --email jeandedieuuwizeye6@gmail.com --noinput
+# DJANGO_SUPERUSER_PASSWORD=1234567890
+# python manage.py createsuperuser --no-input --username admin --email jeandedieuuwizeye6@gmail.com --noinput
